ab964307bc8bbe04476b7ae935c48030f6aba762
Shaddi H <shaddih1@gmail.com>
2020-01-10 13:13:05 -0600

    Update lab.py

--- lab.py
+++ lab.py
@@ -10,7 +10,8 @@ from modules.helper import LabTools
 def usage():
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("-q", "--quiet", help="suppress war", action="store_true")
-    parser.add_argument("-t", "--tool", metavar="tool", help="set a tool to start")
+    parser.add_argument("option", choces=['enable_tor','disable_tor','tools','view_mac','change_mac','metasploit','more'],
+        help="set a option to start")
     if len(sys.argv) < 2:
         pass
     return parser.parse_args()
@@ -33,10 +34,10 @@ def header(quiet):
 def main():
     args = usage()
     header(args.quiet)
-    if args.tool:
+    if args.option:
         # Fire!
         lab = LabTools()
-        lab.start(args.tool)
+        lab.start(args.option)
 
 if __name__ == "__main__":
     try:
