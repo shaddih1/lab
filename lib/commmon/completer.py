13cad68d3adce5629bbc86c0b44e88cb597d8576
Shaddi H <shaddih1@gmail.com>
2020-01-15 16:19:26 -0600

    [+] Added
    -  New file completer.py
    -  New file __init__.py in lib
    -  New file __init__.py in common

---
+++ lib/commmon/completer.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+# standar python library
+import platform, os, socket
+from requests import get
+
+class HeaderCompleter:
+    def system():
+        # get the platform
+        system = platform.platform()
+        return system
+
+    def tor():
+        system = platform.system()
+        # check tor status
+        if system == "Linux":
+            status = os.system('service tor status | head -3 | tail -1 | cut -c 13-29')
+        else:
+            status = "Not supported"
+        return status
+
+    def root():
+        # check user
+        try:
+            if os.geteuid() != 0:
+                root = "●red"
+            else:
+                root = "●green"
+        # windows
+        except:
+            root = "Not supported"
+        return root
+
+    def local_ip():
+        # get local ip
+        try:
+            hostname = socket.gethostname()
+            local_ip = socket.gethostbyname(hostname)
+        # It does not work
+        except:
+            local_ip = "●red"
+        return local_ip
+
+    def myip():
+        # get public ip
+        try:
+            myip = get('https://api.ipify.org').text
+            # It does not work
+        except:
+            myip = "[!] Error"
+        return myip
