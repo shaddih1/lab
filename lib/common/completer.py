ba080a0a685f47fccda001840b1d5aa4b841a415
Shaddi H <shaddih1@gmail.com>
2020-01-17 17:42:41 -0600

    Update completer.py

--- lib/common/completer.py
+++ lib/common/completer.py
@@ -3,19 +3,3 @@
 # standar python library
 import platform, os, socket
 from requests import get
-
-class HeaderCompleter:
-
-    def header(quiet):
-        if not quiet:
-            print("""
-  ██▓    ▄▄▄       ▄▄▄▄
- ▓██▒   ▒████▄    ▓█████▄    By: Shady H
- ▒██░   ▒██  ▀█▄  ▒██▒ ▄██   Version : v0.0.1
- ▒██░   ░██▄▄▄▄██ ▒██░█▀     Twitter: @ShaddiH1
- ░██████▒▓█   ▓██▒░▓█  ▀█▓
- ░ ▒░▓  ░▒▒   ▓▒█░░▒▓███▀▒
- ░ ░ ▒  ░ ▒   ▒▒ ░▒░▒   ░
-   ░ ░    ░   ▒    ░    ░
-     ░  ░     ░  ░ ░
-                        ░""")
