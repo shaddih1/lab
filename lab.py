4bc329b684e283dad8470b5d55d496a2e565442f
Shaddi H <shaddih1@gmail.com>
2020-01-16 21:07:08 -0600

    Update lab.py

--- lab.py
+++ lab.py
@@ -10,7 +10,7 @@ from lib.common.completer import HeaderCompleter
 
 def usage():
     """where we parse all our arguments"""
-    parser = argparse.ArgumentParser(description="[*] Lab - Simplify your life - Ethical Hacking tool")
+    parser = argparse.ArgumentParser(description="Lab - Simplify your life - Ethical Hacking tool")
     parser.add_argument("-q", "--quiet", help="suppress war", action="store_true")
     parser.add_argument("--option", metavar="OPTION", default=None
         ,help="set an option to start")
@@ -38,7 +38,7 @@ def main():
     header(args.quiet)
     if args.option:
         # Fire!
-        lab = LabConductor()
+        lab = orchestra.LabConductor()
         lab.start(args.option)
 
 if __name__ == "__main__":
