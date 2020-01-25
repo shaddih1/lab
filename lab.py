ebe780df4399b357c8dc4f9a8e37f24dd317a492
Shaddi H <shaddih1@gmail.com>
2020-01-25 15:56:03 -0600

    Update lab.py

--- lab.py
+++ lab.py
@@ -18,7 +18,7 @@ from lib.common import helpers
 
 def usage():
 	"""where we parse all our arguments"""
-	parser = argparse.ArgumentParser(description="Lab | Simplify your life | Ethical Hacking tool")
+	parser = argparse.ArgumentParser(description="Lab | Simplify your life | Ethical Hacking toolkit")
 	parser.add_argument("-q", "--quiet", help="suppress header", action="store_true")
 	parser.add_argument("-u", "--update", help="check if there is a new update", action="store_true")
 	parser.add_argument("--list-options", help="list Lab's options", action="store_true")
