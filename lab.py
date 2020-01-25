f6ec8ffd41559d7a248d5dbd208c00e9d1340e35
Shaddi H <shaddih1@gmail.com>
2020-01-25 13:59:15 -0600

    Update lab.py

--- lab.py
+++ lab.py
@@ -14,17 +14,14 @@ if not sys.version_info.major == 3 and sys.version_info.minor >= 6:
 # local lab classes
 from lib.common import orchestra
 from lib.common import message
-from lib.common import helper
+from lib.common import helpers
 
 def usage():
 	"""where we parse all our arguments"""
 	parser = argparse.ArgumentParser(description="Lab | Simplify your life | Ethical Hacking tool")
-	parser.add_argument("-q", "--quiet", help="suppress header"
-		, action="store_true")
-	parser.add_argument("-u", "--update", help="check if there is a new update"
-		, action="store_true")
-	parser.add_argument("--list-options", help="list Lab's options"
-		, action="store_true")
+	parser.add_argument("-q", "--quiet", help="suppress header", action="store_true")
+	parser.add_argument("-u", "--update", help="check if there is a new update", action="store_true")
+	parser.add_argument("--list-options", help="list Lab's options", action="store_true")
 	parser.add_argument("--option", help="set an option to start"
 		, metavar="OPTION")
 
@@ -35,7 +32,7 @@ def main():
 	message.header(args.quiet)
 	lab = orchestra.Conductor()
 	if args.update:
-		helper.update()
+		helpers.update()
 	# --list-options
 	if args.list_options:
 		lab.list_options()
