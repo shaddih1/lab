b0fbb19942010b6ba2c9abe504fd5eabcb25c8a2
Shaddi H <shaddih1@gmail.com>
2020-02-04 15:35:51 -0600

    Fixed tabs

--- lib/common/main.py
+++ lib/common/main.py
@@ -9,6 +9,7 @@ from lib.common import helpers
 from lib.common import message
 
 class MainMenu:
+
     f"""\n┌──[{self.home_dir}]─[/lab/menu]
 └──╼ $
 
@@ -17,70 +18,71 @@ class MainMenu:
         3 - MAC address  |  8 - Credits
         4 - Metasploit   |  Exit
         5 - Update       |  """
-        def __init__(self):
-            self.home_dir = helpers.get_home_dir()
-            self.header = message.header(False)
-            self.menu = MainMenu.__doc__
-            self.prompt = self.start.__doc__
-            self.options = {
-                "1" : self.tor,
-                "2" : self.tools,
-                "3" : self.mac_address,
-                "4" : self.metasploit,
-                "5" : self.update,
-                "6" : self.buy_me_a_coffe,
-                "7" : self.more,
-                "8" : self.credits
-            }
-            self.sleep = time.sleep(1.5)
-            self.clear = os.system('clear')
+    def __init__(self):
+        self.home_dir = helpers.get_home_dir()
+        self.header = message.header(False)
+        self.menu = MainMenu.__doc__
+        self.prompt = self.start.__doc__
+        self.options = {
+            "1" : self.tor,
+            "2" : self.tools,
+            "3" : self.mac_address,
+            "4" : self.metasploit,
+            "5" : self.update,
+            "6" : self.buy_me_a_coffe,
+            "7" : self.more,
+            "8" : self.credits
+        }
+        self.sleep = time.sleep(1.5)
+        self.clear = os.system('clear')
 
-        def start(self):
+    def start(self):
+
+        f"""\n┌──[{self.home_dir}]─[/lab/menu]
+└──╼ $ """
+        exit = False
+        while not exit:
+            header = print(self.header)
+            main_menu = print(self.menu)
+
+            option = input(self.prompt.lower())
+            exit = option == "exit"
+
+            action = self.options.get(option)
+            if action:
+                action()
+            else:
+                error = print("bye")
+                self.sleep
+                self.clear
+
+    def tor(self):
             f"""\n┌──[{self.home_dir}]─[/lab/menu]
-    └──╼ $ """
-            exit = False
-            while not exit:
-                header = print(self.header)
-                main_menu = print(self.menu)
+└──╼ $
 
-                option = input(self.prompt.lower())
-                exit = option == "exit"
+        1 - Enable
+        2 - Disable
+        3 - Status
+        Exit"""
+        print(1)
 
-                action = self.options.get(option)
-                if action:
-                    action()
-                else:
-                    error = print("bye")
-                    self.sleep
-                    self.clear
+    def tools(self):
+        print(2)
 
-        def tor(self):
-            f"""\n┌──[{self.home_dir}]─[/lab/menu]
-        └──╼ $
+    def mac_address(self):
+        print(3)
 
-                1 - Enable
-                2 - Disable
-                3 - Status
-                Exit"""
-            print(1)
+    def metasploit(self):
+        print(4)
 
-        def tools(self):
-            print(2)
+    def update(self):
+        print(5)
 
-        def mac_address(self):
-            print(3)
+    def buy_me_a_coffe(self):
+        print(6)
 
-        def metasploit(self):
-            print(4)
+    def more(self):
+        print(7)
 
-        def update(self):
-            print(5)
-
-        def buy_me_a_coffe(self):
-            print(6)
-
-        def more(self):
-            print(7)
-
-        def credits(self):
-            print(8)
+    def credits(self):
+        print(8)
